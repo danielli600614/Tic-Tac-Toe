31dc2e446eca3595667e13a1f208a08addfe8971
Daniel Li <danielli600614@gmail.com>
2025-12-25 17:38:08 +1100

    First commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+    int boardWidth = 600;
+    int boardHeight = 650; // 50 px for the text pannel on top
+
+    JFrame frame = new JFRame();
+
+    TicTacToe() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight)
+    }
+}
