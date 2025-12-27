01727c91d6a82224c97e446580827792e3cade91
Daniel Li <danielli600614@gmail.com>
2025-12-27 16:28:04 +1100

    feat: Added panels to UI

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,10 +6,63 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeight = 650; // 50 px for the text pannel on top
 
-    JFrame frame = new JFRame();
+    JFrame frame = new JFrame("Tic-Tac-Toe");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
+
 
     TicTacToe() {
         frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight)
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.darkGray);
+        textLabel.setForeground(Color.WHITE);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3, 3));
+        boardPanel.setBackground(Color.darkGray);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                JButton tile = new JButton();
+                board[r][c] = tile;
+                boardPanel.add(tile);
+
+                tile.setBackground(Color.darkGray);
+                tile.setForeground(Color.white);
+                tile.setFont(new Font("Arial",Font.BOLD, 120));
+
+                tile.setOpaque(true);           // 1. Forces the background to be painted
+                tile.setBorderPainted(false);   // 2. Removes the Mac-style rounded border
+
+                tile.setFocusable(false);
+                // tile.setText(currentPlayer);
+
+                tile.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        JButton tile = (JButton) e.getSource();
+                        tile.setText(currentPlayer);
+                    }
+                });
+            }
+        }
     }
 }
